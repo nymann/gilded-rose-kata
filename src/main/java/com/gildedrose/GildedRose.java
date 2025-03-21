e0aed9b288b3970d9aea1b1eb946962a9ef88546
Kristian Jakobsen <kristian@nymann.dev>
2025-03-22 00:43:00 +0100

    Make duplication more clear

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -11,12 +11,12 @@ class GildedRose {
         for (Item item : items) {
             switch (item.name) {
                 case "Aged Brie":
+                    item.sellIn = item.sellIn - 1;
                     if (item.quality < 50) {
                         item.quality = item.quality + 1;
 
                     }
 
-                    item.sellIn = item.sellIn - 1;
 
                     if (item.sellIn < 0) {
                         if (item.quality < 50) {
@@ -26,23 +26,23 @@ class GildedRose {
                     break;
 
                 case "Backstage passes to a TAFKAL80ETC concert":
+                    item.sellIn = item.sellIn - 1;
                     if (item.quality < 50) {
                         item.quality = item.quality + 1;
 
-                        if (item.sellIn < 11) {
+                        if (item.sellIn < 10) {
                             if (item.quality < 50) {
                                 item.quality = item.quality + 1;
                             }
                         }
 
-                        if (item.sellIn < 6) {
+                        if (item.sellIn < 5) {
                             if (item.quality < 50) {
                                 item.quality = item.quality + 1;
                             }
                         }
                     }
 
-                    item.sellIn = item.sellIn - 1;
 
                     if (item.sellIn < 0) {
                         item.quality = 0;
@@ -53,11 +53,12 @@ class GildedRose {
                     break;
 
                 default:
+                    item.sellIn = item.sellIn - 1;
+
                     if (item.quality > 0) {
                         item.quality = item.quality - 1;
                     }
 
-                    item.sellIn = item.sellIn - 1;
 
                     if (item.sellIn < 0) {
                         if (item.quality > 0) {
