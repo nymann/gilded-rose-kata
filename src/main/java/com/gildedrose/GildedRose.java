03a99a40f1eb3f052ea2a5a526773d5372de29dc
Kristian Jakobsen <kristian@nymann.dev>
2025-03-21 17:19:00 +0100

    Extract method 'getItemUpdaterFor'

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -9,15 +9,19 @@ class GildedRose {
 
     public void updateQuality() {
         for (Item item : items) {
-            switch (item.name) {
-                case "Aged Brie" -> new AgedBrieUpdater().update(item);
-                case "Backstage passes to a TAFKAL80ETC concert" ->
-                    new BackstagePassesQualityUpdater().update(item);
-                case "Sulfuras, Hand of Ragnaros" -> {
+            getItemUpdaterFor(item);
+        }
+    }
 
-                }
-                default -> new UncategorizedItemUpdater().update(item);
+    private static void getItemUpdaterFor(Item item) {
+        switch (item.name) {
+            case "Aged Brie" -> new AgedBrieUpdater().update(item);
+            case "Backstage passes to a TAFKAL80ETC concert" ->
+                new BackstagePassesQualityUpdater().update(item);
+            case "Sulfuras, Hand of Ragnaros" -> {
+
             }
+            default -> new UncategorizedItemUpdater().update(item);
         }
     }
 
