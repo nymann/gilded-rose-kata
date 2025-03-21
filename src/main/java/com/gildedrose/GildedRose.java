83add8a8efe9f25306a40abdf9f63acaa689f5db
Kristian Jakobsen <kristian@nymann.dev>
2025-03-21 16:53:31 +0100

    Unwrap if statement

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -50,20 +50,14 @@ class GildedRose {
                 case "Sulfuras, Hand of Ragnaros" -> {
 
                     if (item.sellIn < 0) {
-                        if (!item.name.equals("Aged Brie")) {
-                            if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                                if (item.quality > 0) {
-                                    if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                                        item.quality = item.quality - 1;
-                                    }
+                        if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                            if (item.quality > 0) {
+                                if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                                    item.quality = item.quality - 1;
                                 }
-                            } else {
-                                item.quality = 0;
                             }
                         } else {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1;
-                            }
+                            item.quality = 0;
                         }
                     }
                 }
