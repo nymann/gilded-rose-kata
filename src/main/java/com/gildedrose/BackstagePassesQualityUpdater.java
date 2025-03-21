b941f97e123a0b112adbc9a78c9db538d0f0d82a
Kristian Jakobsen <kristian@nymann.dev>
2025-03-21 22:22:12 +0100

    Make duplication more obvious

--- src/main/java/com/gildedrose/BackstagePassesQualityUpdater.java
+++ src/main/java/com/gildedrose/BackstagePassesQualityUpdater.java
@@ -5,19 +5,18 @@ public class BackstagePassesQualityUpdater implements ItemUpdater {
     }
 
     public void update(Item item) {
+        item.sellIn--;
         if (item.quality < 50) {
             item.quality++;
         }
 
-        if (item.sellIn < 11 && item.quality < 50) {
+        if (item.sellIn < 10 && item.quality < 50) {
             item.quality++;
         }
 
-        if (item.sellIn < 6 && item.quality < 50) {
+        if (item.sellIn < 5 && item.quality < 50) {
             item.quality++;
         }
-
-        item.sellIn--;
 
         if (item.sellIn < 0) {
             item.quality = 0;
