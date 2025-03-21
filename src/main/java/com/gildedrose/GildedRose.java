79bf33b29c2ceb700e9a0d0fff71ad2ecf421984
Kristian Jakobsen <kristian@nymann.dev>
2025-03-21 17:26:02 +0100

    Extract variable 'itemUpdater'

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -9,7 +9,7 @@ class GildedRose {
 
     public void updateQuality() {
         for (Item item : items) {
-            getItemUpdaterFor(item);
+            ItemUpdater itemUpdater = getItemUpdaterFor(item);
         }
     }
 
