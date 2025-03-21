2c53c8878ce6f33ccb930bf22ca71665bb59bae7
Kristian Jakobsen <kristian@nymann.dev>
2025-03-21 16:59:02 +0100

    Merge nested if's

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -27,10 +27,8 @@ class GildedRose {
 
         item.sellIn = item.sellIn - 1;
 
-        if (item.sellIn < 0) {
-            if (item.quality > 0) {
-                item.quality = item.quality - 1;
-            }
+        if (item.sellIn < 0 && item.quality > 0) {
+            item.quality = item.quality - 1;
         }
     }
 
