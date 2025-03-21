641f6a0841020763f3ad4a86e9733b2d8d830732
Kristian Jakobsen <kristian@nymann.dev>
2025-03-21 22:22:27 +0100

    Make duplication more obvious

--- src/main/java/com/gildedrose/UncategorizedItemUpdater.java
+++ src/main/java/com/gildedrose/UncategorizedItemUpdater.java
@@ -6,11 +6,10 @@ public class UncategorizedItemUpdater implements ItemUpdater {
 
     @Override
     public void update(Item item) {
+        item.sellIn--;
         if (item.quality > 0) {
             item.quality--;
         }
-
-        item.sellIn--;
 
         if (item.sellIn < 0 && item.quality > 0) {
             item.quality--;
