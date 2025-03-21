1fa3c6bda28895ee52e2867b936e30a51861aa86
Kristian Jakobsen <kristian@nymann.dev>
2025-03-21 16:53:12 +0100

    Remove if statement

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -51,10 +51,6 @@ class GildedRose {
                     if (item.quality > 0) {
                     }
 
-                    if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                        item.sellIn = item.sellIn - 1;
-                    }
-
                     if (item.sellIn < 0) {
                         if (!item.name.equals("Aged Brie")) {
                             if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
