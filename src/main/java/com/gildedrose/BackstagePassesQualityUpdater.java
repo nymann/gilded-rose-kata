3c6f2ed8fb8fa2baa82c370a3b6a527be47be9a7
Kristian Jakobsen <kristian@nymann.dev>
2025-03-21 22:20:58 +0100

    Split if's

--- src/main/java/com/gildedrose/BackstagePassesQualityUpdater.java
+++ src/main/java/com/gildedrose/BackstagePassesQualityUpdater.java
@@ -7,14 +7,14 @@ public class BackstagePassesQualityUpdater implements ItemUpdater {
     public void update(Item item) {
         if (item.quality < 50) {
             item.quality++;
+        }
 
-            if (item.sellIn < 11 && item.quality < 50) {
-                item.quality++;
-            }
+        if (item.sellIn < 11 && item.quality < 50) {
+            item.quality++;
+        }
 
-            if (item.sellIn < 6 && item.quality < 50) {
-                item.quality++;
-            }
+        if (item.sellIn < 6 && item.quality < 50) {
+            item.quality++;
         }
 
         item.sellIn--;
