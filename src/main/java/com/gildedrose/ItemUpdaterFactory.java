6b17f6ab9291a85e8ced23424475381902b35577
Kristian Jakobsen <kristian@nymann.dev>
2025-03-22 01:21:02 +0100

    Replace with switch expression

--- src/main/java/com/gildedrose/ItemUpdaterFactory.java
+++ src/main/java/com/gildedrose/ItemUpdaterFactory.java
@@ -2,28 +2,18 @@ package com.gildedrose;
 
 public class ItemUpdaterFactory {
     static ItemUpdater create(Item item) {
-        ItemUpdater itemUpdater;
-        switch (item.name) {
-            case "Aged Brie":
-                itemUpdater = new ItemUpdater(new AgedBrieUpdateQualityStrategy(), new DecrementingSellInStrategy());
-                break;
+        ItemUpdater itemUpdater = switch (item.name) {
+            case "Aged Brie" -> new ItemUpdater(new AgedBrieUpdateQualityStrategy(), new DecrementingSellInStrategy());
+            case "Backstage passes to a TAFKAL80ETC concert" ->
+                    new ItemUpdater(new BackstagePassesUpdateQualityStrategy(), new DecrementingSellInStrategy());
+            case "Sulfuras, Hand of Ragnaros" -> new ItemUpdater(new UpdateQualityStrategy() {
+                @Override
+                public void updateQuality(Item item) {
 
-            case "Backstage passes to a TAFKAL80ETC concert":
-                itemUpdater = new ItemUpdater(new BackstagePassesUpdateQualityStrategy(), new DecrementingSellInStrategy());
-                break;
-
-            case "Sulfuras, Hand of Ragnaros":
-                itemUpdater = new ItemUpdater(new UpdateQualityStrategy() {
-                    @Override
-                    public void updateQuality(Item item) {
-
-                    }
-                }, new DoNothingSellInStrategy());
-                break;
-
-            default:
-                itemUpdater = new ItemUpdater(new NormalItemUpdateQualityStrategy(), new DecrementingSellInStrategy());
-        }
+                }
+            }, new DoNothingSellInStrategy());
+            default -> new ItemUpdater(new NormalItemUpdateQualityStrategy(), new DecrementingSellInStrategy());
+        };
         return itemUpdater;
     }
 }
