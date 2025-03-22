1ea1a1245c5f1b371ad68d8276131f50b56d3282
Kristian Jakobsen <kristian@nymann.dev>
2025-03-22 01:21:55 +0100

    Replace with named inner class

--- src/main/java/com/gildedrose/ItemUpdaterFactory.java
+++ src/main/java/com/gildedrose/ItemUpdaterFactory.java
@@ -6,13 +6,15 @@ public class ItemUpdaterFactory {
             case "Aged Brie" -> new ItemUpdater(new AgedBrieUpdateQualityStrategy(), new DecrementingSellInStrategy());
             case "Backstage passes to a TAFKAL80ETC concert" ->
                     new ItemUpdater(new BackstagePassesUpdateQualityStrategy(), new DecrementingSellInStrategy());
-            case "Sulfuras, Hand of Ragnaros" -> new ItemUpdater(new UpdateQualityStrategy() {
-                @Override
-                public void updateQuality(Item item1) {
-
-                }
-            }, new DoNothingSellInStrategy());
+            case "Sulfuras, Hand of Ragnaros" -> new ItemUpdater(new DoNothingQualityStrategy(), new DoNothingSellInStrategy());
             default -> new ItemUpdater(new NormalItemUpdateQualityStrategy(), new DecrementingSellInStrategy());
         };
+    }
+
+    private static class DoNothingQualityStrategy implements UpdateQualityStrategy {
+        @Override
+        public void updateQuality(Item item1) {
+
+        }
     }
 }
