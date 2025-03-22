b2ea2c7debd72f6e6f93cccfe2fad11edcb57800
Kristian Jakobsen <kristian@nymann.dev>
2025-03-22 01:21:05 +0100

    Inline variable 'itemUpdater'

--- src/main/java/com/gildedrose/ItemUpdaterFactory.java
+++ src/main/java/com/gildedrose/ItemUpdaterFactory.java
@@ -2,18 +2,17 @@ package com.gildedrose;
 
 public class ItemUpdaterFactory {
     static ItemUpdater create(Item item) {
-        ItemUpdater itemUpdater = switch (item.name) {
+        return switch (item.name) {
             case "Aged Brie" -> new ItemUpdater(new AgedBrieUpdateQualityStrategy(), new DecrementingSellInStrategy());
             case "Backstage passes to a TAFKAL80ETC concert" ->
                     new ItemUpdater(new BackstagePassesUpdateQualityStrategy(), new DecrementingSellInStrategy());
             case "Sulfuras, Hand of Ragnaros" -> new ItemUpdater(new UpdateQualityStrategy() {
                 @Override
-                public void updateQuality(Item item) {
+                public void updateQuality(Item item1) {
 
                 }
             }, new DoNothingSellInStrategy());
             default -> new ItemUpdater(new NormalItemUpdateQualityStrategy(), new DecrementingSellInStrategy());
         };
-        return itemUpdater;
     }
 }
